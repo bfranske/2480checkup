ed04b81a0e0cf64089c394db8b2ce8389b550e11
Ben Franske <ben.mm@franske.com>
2024-12-18 10:12:07 -0600

    initial script load

---
+++ sbacheck.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+# ITC2480 skills exam checkup script
+import subprocess
+import json
+import re
+from datetime import datetime
+import socket
+
+def getInterfaceDetails():
+    ipData = {}
+    allIntData = json.loads(subprocess.run(["ip", "-j", "addr", "show"], text=True, capture_output=True).stdout))
+    for iface in allIntData:
+        if iface.get('ifname'):
+            intName = iface.get('ifname')
+            ipData[intName] = {}
+            for addr_info in iface.get('addr_info', []):
+                if addr_info.get('family') == 'inet':
+                    ipData[intName].update(ipv4 = addr_info.get('local'))
+                    ipData[intName].update(ipv4prefix = str(addr_info.get('prefixlen')))
+            ipData[intName].update(state = iface.get('operstate'))
+    return(ipData)
+
+def getRootFSCreationDate():
+    partition = '/dev/sda1'
+    tuneOutput = subprocess.run(['tune2fs', '-l', partition], text=True, capture_output=True).stdout
+    pattern = r"^Filesystem created:\s+(.*)$"
+    match = re.search(pattern, tuneOutput, re.MULTILINE)
+    if match:
+        creation_time = match.group(1)
+        date_format = "%a %b %d %H:%M:%S %Y"
+        date_obj = datetime.strptime(creation_time, date_format)
+        #print("Filesystem created:", date_obj)
+        return date_obj
+    return None
+
+def systemDomainName():
+    try:
+        # Get the fully qualified domain name
+        fqdn = socket.getfqdn()
+        return fqdn
+    except Exception as e:
+        return str(e)
+
+def testRootPassword(password):
+    #Currently always returns true because the whole script runs as root
+    result = subprocess.run(['su', '-c', 'exit'], input=password, text=True, capture_output=True)
+    print(result)
+    if result.returncode == 0:
+        print("Password is correct.")
+        return True
+    else:
+        print("Password is incorrect.")
+        return False
+
+ipDetails = getInterfaceDetails()
+systemSetupDate = getRootFSCreationDate()
+hostname = subprocess.run(['hostname'], capture_output=True).stdout.decode('utf-8')
+domainname = systemDomainName()
+testRootPassword('r00tp@ssa')
+
+#test=json.loads(subprocess.run(["ip", "-j", "addr", "show"], capture_output=True).stdout)
+#print(test)
