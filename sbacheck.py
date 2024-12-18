d5bdcf95225bdeaaac07cdf5d66425c8aaa1464b
Ben Franske <ben.mm@franske.com>
2024-12-18 12:27:16 -0600

    add sudo checking

--- sbacheck.py
+++ sbacheck.py
@@ -18,6 +18,9 @@ def getInterfaceDetails():
                 if addr_info.get('family') == 'inet':
                     ipData[intName].update(ipv4 = addr_info.get('local'))
                     ipData[intName].update(ipv4prefix = str(addr_info.get('prefixlen')))
+                else:
+                    ipData[intName].update(ipv4 = None)
+                    ipData[intName].update(ipv4prefix = None)
             ipData[intName].update(state = iface.get('operstate'))
     return(ipData)
 
@@ -46,11 +49,42 @@ def testPassword(username, password):
     p = pam.pam()
     return p.authenticate(username, password, service='login', resetcreds=True)
 
+def checkSudo(username):
+    sudoPermissions = subprocess.run(['sudo', '-l', '-U', username], text=True, capture_output=True)
+    pattern = r"(is not allowed to run sudo|unknown user)"
+    if re.search(pattern, sudoPermissions.stdout) or re.search(pattern, sudoPermissions.stderr):
+        return False
+    else:
+        return True
+
 ipDetails = getInterfaceDetails()
 systemSetupDate = getRootFSCreationDate()
 hostname = subprocess.run(['hostname'], capture_output=True).stdout.decode('utf-8')
 domainname = systemDomainName()
-print(testPassword('root','r00tp@ssa'))
+rootPasswordTest = testPassword('root','r00tp@ss')
+examPasswordTest = testPassword('examuser','GoodLuck')
+examSudoTest = checkSudo('examuser')
+linuxPasswordTest = testPassword('linuxgeek','linuxi$fun!')
+linuxSudoTest = checkSudo('linuxgeek')
+
+print("------------------------------")
+print("System Report:")
+print("------------------------------")
+print(f"The system was installed on: {systemSetupDate}")
+print("------------------------------")
+print("Part 2:")
+print("------------------------------")
+print(f"ens192 is {ipDetails['ens192']['state']} with IP Address: {ipDetails['ens192']['ipv4']}/{ipDetails['ens192']['ipv4prefix']}")
+print(f"The system host name is {hostname}")
+print(f"The system domain name is {domainname}")
+print(f"The root user account has the right password: {rootPasswordTest}")
+print("------------------------------")
+print("Part 3:")
+print("------------------------------")
+print(f"The examuser user account has the right password: {examPasswordTest}")
+print(f"The examuser user account has sudo: {examSudoTest}")
+print(f"The linuxgeek user account has the right password: {linuxPasswordTest}")
+print(f"The linuxgeek user account has sudo: {linuxSudoTest}")
 
 #test=json.loads(subprocess.run(["ip", "-j", "addr", "show"], capture_output=True).stdout)
 #print(test)
